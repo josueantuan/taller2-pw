17926e9d85d64e31330e202e496837055aad11f0
antuan barahona <antuan_1996@hotmail.com>
2018-09-27 15:22:36 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddDatosComponent } from './components/add-datos/add-datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddDatosComponent
   ],
   imports: [
     BrowserModule
